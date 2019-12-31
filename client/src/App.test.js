a917f41fbb2120fb1a3e69030c8d9f06303253fa
bpremkumar <bpremkumar@gmail.com>
2019-12-30 21:03:24 -0600

    initial commit

--- client/src/App.test.js
+++ client/src/App.test.js
@@ -2,11 +2,4 @@ import React from 'react';
 import ReactDOM from 'react-dom';
 import App from './App';
 
-/* This is commented to prevent the service from failing
-it('renders without crashing', () => {
-  const div = document.createElement('div');
-  ReactDOM.render(<App />, div);
-  ReactDOM.unmountComponentAtNode(div);
-}); */
-
 it('renders without crashing', () => {});
